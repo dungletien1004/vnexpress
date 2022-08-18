0fe21880f07bf1beff4befe77f65d9efbac045ea
dungletien1305 <dung.letien1305@gmail.com>
2022-08-19 00:37:42 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { publishRoutes } from '~/routes';
 import { DefaultLayout } from './components/Layout';
+import Home from './pages/Home/Home';
 
 function App() {
+    document.title = 'VnExpress';
     return (
         <Router>
             <div className="App">
-                <Routes>
-                    {publishRoutes.map((route, index) => {
-                        const Layout = DefaultLayout;
-                        const Page = route.component;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
+                {/* <Routes>
+                {publishRoutes.map((route, index) => {
+                    const Layout = DefaultLayout;
+                    const Page = route.component;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes> */}
+                <DefaultLayout>
+                    <Home />
+                </DefaultLayout>
             </div>
         </Router>
     );
